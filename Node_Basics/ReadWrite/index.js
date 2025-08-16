7ec9f0dd1fa0156ae13db996ada2c5055714bff7
Aditya <adiu9755@gmail.com>
2025-08-16 21:06:31 +0530

    Write async example

--- Node_Basics/ReadWrite/index.js
+++ Node_Basics/ReadWrite/index.js
@@ -1,7 +1,17 @@
 const fs = require('fs')
 
-const inputText = fs.readFileSync('./inputText.txt','utf-8');
 
-console.log(inputText);
-const textToWrite = ` Output Text ${inputText}.\n Created on ${Date.now()}`
-fs.writeFileSync('./Output.text', textToWrite)
+//Sync
+const inputText = fs.readFileSync('./inputText.txt','utf-8');
+// console.log(inputText);
+
+// const textToWrite = ` Output Text ${inputText}.\n Created on ${Date.now()}`
+// fs.writeFileSync('./Output.text', textToWrite)
+
+
+//Async
+
+fs.writeFile('./writeSync.txt',inputText,(err)=>{
+  console.log(data);
+});
+console.log("Execute first");
