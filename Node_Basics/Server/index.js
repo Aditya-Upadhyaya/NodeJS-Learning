52e1f8320e46826aabf2e147f7409ed4aae98bbd
Aditya <adiu9755@gmail.com>
2025-12-08 10:57:44 +0530

    http example

--- Node_Basics/Server/index.js
+++ Node_Basics/Server/index.js
@@ -1,27 +1,22 @@
-const http = require('http')
-const url = require('url')
+const http = require("http");
+const url = require("url");
 
-const server = http.createServer((req, res)=>{
-   let path = req.url;
-   console.log(path);
-   if (path=='/' || path=='/overview') {
-    res.end('Message from overview');
-   }
-   else if(path == '/product'){
-    res.end('Message from product');
-   }
-   else{
-    res.writeHead(404,{
-        'Content-type':'text/html',
-        'my-header':'my custome header'
-    })
-    res.end('<h1>Page not found</h1>');
-   }
+const server = http.createServer((req, res) => {
+  let path = req.url;
+  console.log(path);
+  if (path == "/" || path == "/overview") {
+    res.end("Message from overview");
+  } else if (path == "/product") {
+    res.end("Message from product");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-header": "my custome header",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
+});
 
-
-   
-})
-
-server.listen(8080, '127.0.0.1', ()=>{
-    console.log("Server started...");
-})
+server.listen(8080, "127.0.0.1", () => {
+  console.log("Server started...");
+});
